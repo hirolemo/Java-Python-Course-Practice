b258e0278e4b713be26a024665fb99e9ccbc280f
Mohamed <hirole.mohamed@gmail.com>
2018-08-18 19:05:56 -0400

    Inner classes, cool function call anon. class instantiation

---
+++ Inner Classes Challenge/src/com/timbuchalka/Album.java
@@ -0,0 +1,97 @@
+package com.timbuchalka;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created by dev on 18/09/15.
+ */
+public class Album {
+    private String name;
+    private String artist;
+    private SongList songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new SongList();
+    }
+
+    public boolean addSong(String title, double duration) {
+        return this.songs.add(new Song(title, duration));
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+        Song checkedSong = this.songs.findSong(trackNumber); //converts trackNumber in findSong  int method
+
+        if(checkedSong!=null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+        Song checkedSong = songs.findSong(title);
+        if(checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return false;
+    }
+
+    public class SongList {
+        private ArrayList<Song> songs = new ArrayList<Song>(); //not used by outer class, but needed for inner methods to work
+
+        public SongList() {
+            this.songs = new ArrayList<Song>();
+        }
+
+        public boolean add(Song song) {
+            if(songs.contains(song)) {
+                return false;
+            }
+            songs.add(song);
+            return true;
+        }
+
+        private Song findSong(String title) {
+            for(Song checkedSong: this.songs) {
+                if(checkedSong.getTitle().equals(title)) {
+                    return checkedSong;
+                }
+            }
+            return null;
+        }
+
+        public Song findSong(int trackNumber) {
+            int index = trackNumber -1;
+            if(index >=0 && (index < songs.size())) {
+
+                //retrieve song as that index
+                return songs.get(index);
+            }
+            return null; //out of bounds index
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
